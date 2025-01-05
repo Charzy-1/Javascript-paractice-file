ca7faf5dbdee521a5ce841a12be85308ceac6ea6
Charles <chazylites@gmail.com>
2025-01-05 14:46:11 +0100

    started function moudule

---
+++ functions/script.js
@@ -0,0 +1,19 @@
+// Functions in Javascript are a block of code that peforms a specific task
+
+/* A FUNCTION DECLARATION (defining a function)
+A simple structure of a function
+
+function name of the function (parameter to pass to the function) {
+    function body which can be anything.;
+} */
+
+// A use case scenario for a function that adds a number
+
+function add (number) {
+    return number + number;
+}
+
+// A FUNCTION CALL (caling or executing a function)
+const result = add(10);
+
+console.log(result)

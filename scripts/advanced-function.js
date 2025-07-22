ee7a676da5094496387ee9b0c1c3a3a63be3e9ca
Charles <chazylites@gmail.com>
2025-07-22 08:51:47 +0100

    Started the advanced function challenge

---
+++ scripts/advanced-function.js
@@ -0,0 +1,27 @@
+// 12a Create a variable called add and save a function inside. This function will console.log(2+3); run the function twice.
+
+const add = function () {
+  console.log(2+3);
+}
+
+add();
+
+// 12b Create a function runTwice(fun) that takes a function as a parameter and rus it runTwice.
+
+const runTwice = (fun) => {
+  fun()
+}
+
+runTwice(add);
+
+// 12c Create a button when clicking the button, after 3 seconds, the text insede the button cnanges to 'Finished'. 
+
+const jsButton = document.querySelector('.finish-button');
+
+const buttonElement = () => {
+  setTimeout(() => {
+    jsButton.innerHTML = 'Finished!';
+  }, 3000); 
+};
+
+jsButton.addEventListener('click', buttonElement);

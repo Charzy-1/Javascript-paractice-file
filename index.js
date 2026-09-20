1cf109e757f841ded2126fc4538c2c795c0ddc4f
Charles Adikankwu <chazylites@gmail.com>
2026-09-20 11:20:21 +0100

    Created a funtion to pick computer move

--- index.js
+++ index.js
@@ -2,3 +2,21 @@
 
 let cart_quantity = 0;
 
+// Function that will pick computer move
+
+// First declare the global variable
+let computerMove = '';
+
+let result = '';
+
+function pickComputerMove() {
+  const randomNumber =  Math.random();
+  
+  if (randomNumber >= 0 && randomNumber < 1/3) {
+    computerMove = 'Rock';
+  } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+    computerMove = 'Paper';
+  } else if (randomNumber >= 2/3 && randomNumber < 1) {
+    computerMove = 'Scissors';
+  }
+}

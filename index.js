0a48bf5bba58e924e236567dd5dc949dfe132577
Charles Adikankwu <chazylites@gmail.com>
2026-09-20 18:07:43 +0100

    Added the reset button and making it interactive

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // This is a comment
-
 let cart_quantity = 0;
+
+
+// Creating an object to add a score by updating the result.
+const score = {
+  wins: 0,
+  losses: 0,
+  ties: 0
+}
 
 // Function PLAYER MOVE
 
@@ -11,31 +18,42 @@ function playGame(myMove) {
 
   if (myMove === 'Rock') {
     if (computerMove === 'Rock') {
+      result = 'It is a tie';
+    } else if (computerMove === 'Paper') {
+      result = 'You lose.';
+    } else if (computerMove === 'Scissors') {
+      result = 'You won.';
+    }
+
+  } else if (myMove === 'Paper') {
+    if (computerMove === 'Rock') {
       result = 'You won.';
     } else if (computerMove === 'Paper') {
       result = 'It is a tie';
     } else if (computerMove === 'Scissors') {
-      result = 'You lose';
-    } else if (myMove === 'Paper') {
-      if (computerMove === 'Rock') {
-        result = 'You won.';
-      } else if (computerMove === 'Paper') {
-        result = 'It is a tie';
-      } else if (computerMove === 'Scissors') {
-        result = 'You lose';
-      }
-    } else if (myMove === 'Scissors') {
-      if (computerMove === 'Rock') {
       result = 'You lose.';
-      } else if (computerMove === 'Paper') {
-        result = 'You won.';
-      } else if (computerMove === 'Scissors') {
-        result = 'it is a tie.';
-      }
+    }
+
+  } else if (myMove === 'Scissors') {
+    if (computerMove === 'Rock') {
+      result = 'You lose.';
+    } else if (computerMove === 'Paper') {
+      result = 'You won.';
+    } else if (computerMove === 'Scissors') {
+      result = 'It is a tie';
     }
   }
 
-  alert(`You picked ${myMove}, Computer picked ${computerMove}. ${result}.`);
+  if (result === 'You won.') {
+    score.wins += 1;
+  } else if (result === 'You lose.') {
+    score.losses += 1;
+  } else if (result === 'It is a tie') {
+    score.ties += 1;
+  } 
+
+  alert(`You picked ${myMove}, Computer picked ${computerMove}. ${result}.
+Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}.`);
 }
 
 // FUNCTION FOR THE COMPUTERMOVE
@@ -53,4 +71,14 @@ function pickComputerMove() {
   }
   
   return computerMove;
+}
+
+// FUNCTION TO RESET THE BUTTON
+function resetScore() {
+  score.wins = 0
+  score.losses = 0
+  score.ties = 0
+
+  alert(`Scores have been reset
+Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}.`)
 }
